914070f0f347aa718097111a21836c6c603c4cd4
Philip White <PW259246@falmouth.ac.uk>
2021-11-01 19:32:04 +0000

    Added Enemy Return Script Functionality

--- Unity-Files/Assets/Scripts/Movement/EnemyReturn.cs
+++ Unity-Files/Assets/Scripts/Movement/EnemyReturn.cs
@@ -5,15 +5,52 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class EnemyReturn : Physics2DObject
 {
+    [Header("Movement Speed")]
+    [Tooltip("The amount of speed in which the boat moves towards the castle")]
+    public float normalSpeed = 5f;
+    [Tooltip("The amount of speed in which the boat moves away from the castle")]
+    public float returnSpeed = 5f;
+
+    private bool isReturning = false;
+    private Vector2 movement = new Vector2(0f, 0f);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isReturning)
+        {
+            movement = new Vector2(0f, returnSpeed);
+        }
+        else
+        {
+            movement = new Vector2(0f, -normalSpeed);
+        }
+    }
+
+    //FixedUpdate is called every frame when the physics are calculated
+    void FixedUpdate()
+    {
+        rigidbody2D.AddForce(movement);
+    }
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        string playerTag = otherCollider.gameObject.tag;
+        if (playerTag == "Player" || playerTag == "Player2")
+        {
+            HealthSystemAttribute healthScript = otherCollider.gameObject.GetComponent<HealthSystemAttribute>();
+            if (healthScript != null)
+            {
+                // subtract health from the player
+                healthScript.ModifyHealth(-1);
+            }
+            this.GetComponent<SpriteRenderer>().flipY = false;
+            isReturning = true;
+        }
     }
 }

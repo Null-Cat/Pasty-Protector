05311b5b2dd925c96c4c249f33846d03c6791c44
Philip White <PW259246@falmouth.ac.uk>
2021-11-01 20:09:52 +0000

    Update Self Destruct Script

--- Unity-Files/Assets/Scripts/Gameplay/TimedSelfDestructWithTimer.cs
+++ Unity-Files/Assets/Scripts/Gameplay/TimedSelfDestructWithTimer.cs
@@ -1,29 +1,37 @@
 using UnityEngine;
 using System.Collections;
 
-public class TimedSelfDestruct : MonoBehaviour
+public class TimedSelfDestructWithTimer : MonoBehaviour
 {
 
-	// After this time, the object will be destroyed
-	public float timeToDestruction;
+    // After this time, the object will be destroyed
+    public float timeToDestruction;
 
 
-	void Start ()
-	{
-		Invoke("DestroyMe", timeToDestruction);
-	}
+    void Start()
+    {
+        //Invoke("DestroyMe", timeToDestruction);
+    }
 
     // Update is called every frame, if the MonoBehaviour is enabled.
     void Update()
     {
-        
+        if (timeToDestruction > 0)
+        {
+            timeToDestruction -= Time.deltaTime;
+            GetComponentInChildren<TextMesh>().text = Mathf.Abs(timeToDestruction).ToString();
+        }
+        else
+        {
+            DestroyMe();
+        }
     }
 
-	// This function will destroy this object :(
-	void DestroyMe()
-	{
-		Destroy(gameObject);
+    // This function will destroy this object :(
+    void DestroyMe()
+    {
+        Destroy(gameObject);
 
-		// Bye bye!
-	}
+        // Bye bye!
+    }
 }

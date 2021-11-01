eea58ac4472f70c8c0005046745c9a0b74be54e4
Philip White <PW259246@falmouth.ac.uk>
2021-11-01 20:18:50 +0000

    Update Enemy Return Func & Pastie Spawning

--- Unity-Files/Assets/Scripts/Movement/EnemyReturn.cs
+++ Unity-Files/Assets/Scripts/Movement/EnemyReturn.cs
@@ -10,6 +10,10 @@ public class EnemyReturn : Physics2DObject
     public float normalSpeed = 5f;
     [Tooltip("The amount of speed in which the boat moves away from the castle")]
     public float returnSpeed = 5f;
+    [Header("Dropped Object")]
+    public GameObject droppedObject;
+    [Header("Death Effect When Shot")]
+    public GameObject deathEffect;
 
     private bool isReturning = false;
     private Vector2 movement = new Vector2(0f, 0f);
@@ -43,14 +47,28 @@ public class EnemyReturn : Physics2DObject
         string playerTag = otherCollider.gameObject.tag;
         if (playerTag == "Player" || playerTag == "Player2")
         {
-            HealthSystemAttribute healthScript = otherCollider.gameObject.GetComponent<HealthSystemAttribute>();
-            if (healthScript != null)
-            {
-                // subtract health from the player
-                healthScript.ModifyHealth(-1);
-            }
+            // HealthSystemAttribute healthScript = otherCollider.gameObject.GetComponent<HealthSystemAttribute>();
+            // if (healthScript != null)
+            // {
+            //     // subtract health from the player
+            //     healthScript.ModifyHealth(-1);
+            // }
             this.GetComponent<SpriteRenderer>().flipY = false;
             isReturning = true;
+        }
+        else if (playerTag == "Bullet")
+        {
+            if (deathEffect != null)
+            {
+                GameObject newDeathEffect = Instantiate<GameObject>(deathEffect);
+                newDeathEffect.transform.position = this.transform.position;
+            }
+            float randomX = Random.Range(0, 1);
+            float randomY = Random.Range(0, 1);
+            GameObject newDroppedObject = Instantiate<GameObject>(droppedObject);
+            newDroppedObject.transform.position = new Vector2(randomX + this.transform.position.x, randomY + this.transform.position.y);
+
+            Destroy(gameObject);
         }
     }
 }

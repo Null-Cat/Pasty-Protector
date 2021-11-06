f5f3953f9e1ad7140ed004e765f3804b80efc841
Philip White <PW259246@falmouth.ac.uk>
2021-11-06 06:44:20 +0000

    Update UIScript, Leaderboard Handler For Database

--- Unity-Files/Assets/Scripts/Menu/LeaderboardHandle.cs
+++ Unity-Files/Assets/Scripts/Menu/LeaderboardHandle.cs
@@ -6,7 +6,14 @@ using MySql.Data.MySqlClient;
 using System.Net;
 public class LeaderboardHandle : MonoBehaviour
 {
+    public InputField submitUsername;
+    public GameObject GameOverPanel;
+    public float time { get; set; }
+    public int finalScore { get; set; }
+
     private MySqlConnectionStringBuilder mySQLConectionBuilder = new MySqlConnectionStringBuilder();
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,23 +22,30 @@ public class LeaderboardHandle : MonoBehaviour
         mySQLConectionBuilder.UserID = "sql4449219";
         mySQLConectionBuilder.Password = "hsFqWLxIIF";
         mySQLConectionBuilder.Database = "sql4449219";
-
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SubmitScores()
     {
+        SetScoreRecords(mySQLConectionBuilder.ConnectionString, submitUsername.text);
+        GameOverPanel.GetComponent<Animator>().enabled = true;
 
+        List<Score> Scores = new List<Score>(GetScoreRecords(mySQLConectionBuilder.ConnectionString));
+        for (int i = 0; i < Scores.Count; i++)
+        {
+            Text scoreLabel = GameObject.Find($"ScoreText ({i + 1})").GetComponent<Text>();
+            string editedText = scoreLabel.text.Replace("AAA", $"{Scores[i].username}").Replace("111", $"{Scores[i].score}").Replace("222", $"{Scores[i].timeLasted}");
+            scoreLabel.text = editedText;
+        }
     }
 
-    private void SetScoreRecords(string connectionString, string username, int score, int timeLasted)
+    private void SetScoreRecords(string connectionString, string username)
     {
         try
         {
             using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
                 connection.Open();
-                string sql = $"INSERT INTO Scores VALUES ('{username}', '{score}', '{timeLasted}', '{GetIPAddress()}')";
+                string sql = $"INSERT INTO Scores VALUES ('{username.ToUpper()}', '{finalScore}', '{Mathf.FloorToInt(time)}', '{GetIPAddress()}')";
                 using (MySqlCommand command = new MySqlCommand(sql, connection))
                 {
                     command.ExecuteNonQuery();
@@ -44,14 +58,14 @@ public class LeaderboardHandle : MonoBehaviour
         }
     }
 
-    private void GetScoreRecords(string connectionString)
+    private List<Score> GetScoreRecords(string connectionString)
     {
+        List<Score> records = new List<Score>();
         try
         {
             using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
                 connection.Open();
-                Debug.Log("connection established");
                 string sql = "SELECT * FROM Scores ORDER BY Score DESC LIMIT 5";
                 using (MySqlCommand command = new MySqlCommand(sql, connection))
                 {
@@ -59,7 +73,7 @@ public class LeaderboardHandle : MonoBehaviour
                     {
                         while (reader.Read())
                         {
-
+                            records.Add(new Score(reader.GetString(0), reader.GetInt32(1), reader.GetInt32(2)));
                         }
                     }
                 }
@@ -69,6 +83,7 @@ public class LeaderboardHandle : MonoBehaviour
         {
             Debug.Log(e.ToString());
         }
+        return records;
     }
 
     public class Score

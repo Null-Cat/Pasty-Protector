e48ca1095d14a1773afd19f80b2b80e680e17bb3
Jamie Leveridge <JL260801@falmouth.ac.uk>
2021-11-11 10:48:49 +0000

    added bomb and nuke power ups

---
+++ Unity-Files/Assets/Scripts/Gameplay/BombPowerUp.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombPowerUp : MonoBehaviour
+{
+    public float explosionRange;
+    public GameObject explosionEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet"))
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(gameObject.transform.position, explosionRange);
+
+        if(colliders.Length > 0)
+        {
+            foreach (Collider2D boats in colliders)
+            {
+                if (boats.CompareTag("Enemy"))
+                {
+                    if (explosionEffect != null)
+                    {
+                        GameObject newDeathEffect = Instantiate<GameObject>(explosionEffect);
+                        newDeathEffect.transform.position = boats.gameObject.transform.position;
+                    }
+                    Destroy(boats.gameObject);
+                }
+            }
+        }
+        Destroy(gameObject);
+    }
+}

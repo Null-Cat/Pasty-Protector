b4273b69f32023d8d396e4dd265b11063b5c601c
Philip White <PW259246@falmouth.ac.uk>
2021-11-14 16:49:45 +0000

    Update Ability to Change Difficulty Scale Type

--- Unity-Files/Assets/Scripts/Gameplay/DifficultyScale.cs
+++ Unity-Files/Assets/Scripts/Gameplay/DifficultyScale.cs
@@ -2,27 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class DifficultyScale : MonoBehaviour
 {
     public GameObject time;
+    public TypeOfDifficulty difficultyType;
     [SerializeField] private int incrementDifficultyInSeconds = 30;
     [SerializeField] private int incrementAmountInSeconds = 25;
     private int currentDifficultyLevel = 0;
-
-    // Start is called before the first frame update
-    void Start()
+    public enum TypeOfDifficulty
     {
-
+        Exponential,
+        Constant
     }
-
     // Update is called once per frame
     void Update()
     {
-        if (((Mathf.FloorToInt(time.GetComponent<UITimer>().time) - (incrementAmountInSeconds * currentDifficultyLevel)) / incrementDifficultyInSeconds) == 1)
+        switch (difficultyType)
         {
-            currentDifficultyLevel++;
-            incrementDifficultyInSeconds += incrementAmountInSeconds;
-            this.GetComponent<ObjectCreatorArea>().spawnInterval = this.GetComponent<ObjectCreatorArea>().spawnInterval * 0.5f;
+            case TypeOfDifficulty.Exponential:
+                if (((Mathf.FloorToInt(time.GetComponent<UITimer>().time) - (incrementAmountInSeconds * currentDifficultyLevel)) / incrementDifficultyInSeconds) == 1)
+                {
+                    currentDifficultyLevel++;
+                    incrementDifficultyInSeconds += incrementAmountInSeconds;
+                    this.GetComponent<ObjectCreatorArea>().spawnInterval = this.GetComponent<ObjectCreatorArea>().spawnInterval * 0.5f;
+                }
+                break;
+            case TypeOfDifficulty.Constant:
+                if ((Mathf.FloorToInt(time.GetComponent<UITimer>().time) >= (incrementDifficultyInSeconds * (currentDifficultyLevel + 1))))
+                {
+                    currentDifficultyLevel++;
+                    this.GetComponent<ObjectCreatorArea>().spawnInterval = this.GetComponent<ObjectCreatorArea>().spawnInterval * 0.5f;
+                }
+                break;
         }
     }
 }

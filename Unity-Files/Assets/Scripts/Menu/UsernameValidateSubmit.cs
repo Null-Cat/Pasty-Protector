1c097ed28a9d4ee0a12f06a2b4fdbc3779717c6a
Philip White <PW259246@falmouth.ac.uk>
2021-11-14 14:33:44 +0000

    Added Visual Response on Invalid Input

--- Unity-Files/Assets/Scripts/Menu/UsernameValidateSubmit.cs
+++ Unity-Files/Assets/Scripts/Menu/UsernameValidateSubmit.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Threading.Tasks;
 public class UsernameValidateSubmit : MonoBehaviour
 {
     private void Start()
@@ -14,6 +15,55 @@ public class UsernameValidateSubmit : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Return) && GetComponent<InputField>().text.Length == 3)
         {
             GameObject.Find("GameOverPanel").GetComponent<LeaderboardHandle>().SubmitScores();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            FlashRed();
+        }
+    }
+
+    public void buttonValidateInput()
+    {
+        if (GetComponent<InputField>().text.Length == 3)
+        {
+            GameObject.Find("GameOverPanel").GetComponent<LeaderboardHandle>().SubmitScores();
+        }
+        else
+        {
+            FlashRed();
+        }
+    }
+
+    public async void FlashRed()
+    {
+        try
+        {
+            if (GetComponent<InputField>().text != "")
+            {
+                Color defaultColor = GetComponent<InputField>().textComponent.color;
+                GetComponent<InputField>().textComponent.color = Color.red;
+                await Task.Delay(100);
+                GetComponent<InputField>().textComponent.color = defaultColor;
+                await Task.Delay(100);
+                GetComponent<InputField>().textComponent.color = Color.red;
+                await Task.Delay(100);
+                GetComponent<InputField>().textComponent.color = defaultColor;
+            }
+            else
+            {
+                Color defaultColor = GetComponent<InputField>().placeholder.color;
+                GetComponent<InputField>().placeholder.color = Color.red;
+                await Task.Delay(100);
+                GetComponent<InputField>().placeholder.color = defaultColor;
+                await Task.Delay(100);
+                GetComponent<InputField>().placeholder.color = Color.red;
+                await Task.Delay(100);
+                GetComponent<InputField>().placeholder.color = defaultColor;
+            }
+        }
+        catch (MissingReferenceException)
+        {
+            Debug.Log("Missing Reference: Ignoring Due to Async Winddown");
         }
     }
 }
